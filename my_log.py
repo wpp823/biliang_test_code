976bb216fb3a0d13bf31ea252bb06456c8e86f0c
wangying <wpp_work_mail@163.com>
2022-01-27 15:25:45 +0800

    日常备份

--- my_log.py
+++ my_log.py
@@ -3,29 +3,32 @@ import logging
 import os
 from logging.handlers import TimedRotatingFileHandler
 
-logger = logging.getLogger(__name__)
-logger.setLevel(level=logging.INFO)
 
-# if not os.path.exists(f'{os.getcwd()}\\bin'):
-#     os.mkdir(f'{os.getcwd()}\\bin')
+def get_logger():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(level=logging.INFO)
 
-# 日志文件夹
-if not os.path.exists(f'{os.getcwd()}\\bin\\logs'): os.mkdir(f'{os.getcwd()}\\bin\\logs')
-log_path = os.getcwd() + '\\bin\\logs\\'
+    # if not os.path.exists(f'{os.getcwd()}\\bin'):
+    #     os.mkdir(f'{os.getcwd()}\\bin')
 
-# # 日志格式
-formatter = '%(asctime)s-%(filename)s-[line]: %(lineno)d-%(levelname)s-%(message)s'
+    # 日志文件夹
+    if not os.path.exists(f'{os.getcwd()}\\bin\\logs'): os.mkdir(f'{os.getcwd()}\\bin\\logs')
+    log_path = os.getcwd() + '\\bin\\logs\\'
 
-# 日志文件按时间切分
-# interval： 指定日志文件轮转的周期，如 when='S', interval=10，表示每10秒轮转一次，when='D', interval=7，表示每周轮转一次。
-# backupCount： 指定日志文件保留的数量，指定一个整数，则日志文件只保留这么多个，自动删除旧的文件
-time_rotate_file = TimedRotatingFileHandler(filename=f'{log_path}log', when='D', interval=1, backupCount=20)
-time_rotate_file.setFormatter(logging.Formatter(formatter, datefmt='%Y-%m-%d %H:%M'))
-time_rotate_file.setLevel(logging.INFO)
+    # # 日志格式
+    formatter = '%(asctime)s-%(filename)s-[line]: %(lineno)d-%(levelname)s-%(message)s'
 
-console_handler = logging.StreamHandler()
-console_handler.setLevel(level=logging.INFO)
-console_handler.setFormatter(logging.Formatter(formatter, datefmt='%Y-%m-%d %H:%M'))
+    # 日志文件按时间切分
+    # interval： 指定日志文件轮转的周期，如 when='S', interval=10，表示每10秒轮转一次，when='D', interval=7，表示每周轮转一次。
+    # backupCount： 指定日志文件保留的数量，指定一个整数，则日志文件只保留这么多个，自动删除旧的文件
+    time_rotate_file = TimedRotatingFileHandler(filename=f'{log_path}log', when='D', interval=1, backupCount=20)
+    time_rotate_file.setFormatter(logging.Formatter(formatter, datefmt='%Y-%m-%d %H:%M'))
+    time_rotate_file.setLevel(logging.INFO)
 
-logger.addHandler(time_rotate_file)
-logger.addHandler(console_handler)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(level=logging.INFO)
+    console_handler.setFormatter(logging.Formatter(formatter, datefmt='%Y-%m-%d %H:%M'))
+
+    logger.addHandler(time_rotate_file)
+    logger.addHandler(console_handler)
+    return logger
