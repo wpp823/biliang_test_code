f012cd08e85e1652d72c26cb3c45cad154d7952f
wangying <wpp_work_mail@163.com>
2022-02-09 18:12:57 +0800

    暂存

--- 体素网页测试demo/sever/main.py
+++ 体素网页测试demo/sever/main.py
@@ -3,6 +3,19 @@ import tornado.web
 
 
 # 定义处理类型
+class TestHandler(tornado.web.RequestHandler):
+    # 添加一个处理get请求方式的方法
+    def get(self):
+        print(self.request.body)
+        # 向响应中，添加数据
+        self.write('好看的皮囊千篇一律，有趣的灵魂万里挑一。')
+
+    def post(self):
+        print(self.request.body)
+        self.write('好看的皮囊千篇一律，有趣的灵魂万里挑一。')
+
+
+
 class IndexHandler(tornado.web.RequestHandler):
     # 添加一个处理get请求方式的方法
     def get(self):
@@ -11,6 +24,7 @@ class IndexHandler(tornado.web.RequestHandler):
 
     def post(self):
         result = self.get_img_ret_data()
+
         self.write(result)
 
     def get_img_ret_data(self):
@@ -27,7 +41,7 @@ class IndexHandler(tornado.web.RequestHandler):
 
 if __name__ == '__main__':
     # 创建一个应用对象
-    app = tornado.web.Application([(r'/', IndexHandler)])
+    app = tornado.web.Application([(r'/test', TestHandler),(r'/upload/img',IndexHandler)])
     # 绑定一个监听端口
     app.listen(8888)
     # 启动web程序，开始监听端口的连接
---
+++ 体素网页测试demo/vue_demo/src/components/upload.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-upload
+    ref="imgUpload"
+    :on-success="imgSuccess"
+    :on-remove="imgRemove"
+    accept="image/gif,image/jpeg,image/jpg,image/png,image/svg"
+    :headers="headerMsg"
+    :action="upLoadUrl"
+    multiple>
+    <el-button type="primary">上传图片</el-button>
+  </el-upload>
+</template>
+
+<script>
+
+export default {
+  name: 'imgUpload',
+  data() {
+    return {
+      // headerMsg: {Token: 'XXXXXX'},
+      upLoadUrl: 'http://127.0.0.1:8888/test'
+    }
+  },
+  methods: {
+    // 上传图片成功
+    imgSuccess(res, file, fileList) {
+      console.log(res)
+      console.log(file)
+      console.log(fileList)  // 这里可以获得上传成功的相关信息
+    }
+  }
+}
+</script>
