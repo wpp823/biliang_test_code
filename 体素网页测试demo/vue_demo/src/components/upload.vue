e752d65d0cc0e2e9f59488119ddc672c90c9121d
wangying <wpp_work_mail@163.com>
2022-02-11 15:44:18 +0800

    修网页打印json显示

--- 体素网页测试demo/vue_demo/src/components/upload.vue
+++ 体素网页测试demo/vue_demo/src/components/upload.vue
@@ -1,9 +1,8 @@
 <template>
-  <div id="app">
+  <div>
     <img :src="imgUrl" width="400" height="300"/><br/>
     <input type="file" @change="tirggerFile($event)" ref="inputer" accept="image/jpeg,image/jpg"/><br/>
     <input type="button" value="上传" @click="uploadpic()"/><br/>
-<!--    <button @click="get_img_info">获取图片</button>-->
     <json-viewer :value="showtext" :expand-depth=4 copyable sort></json-viewer>
 
   </div>
@@ -31,16 +30,9 @@ export default {
       reader.onload = function (e) {
         url = this.result.substring(this.result.indexOf(",") + 1);
         that.imgUrl = "data:image/png;base64," + url;
-        console.log(url)
-        // that.$refs['imgimg'].setAttribute('src','data:image/png;base64,'+url);
       };
-      console.log(32, url)
-
     },
-    // get_img_info() {
-    //   console.log(this.$refs.inputer.files)
-    // },
-    // 上传图片成功
+
     uploadpic() {
       let inputDOM = this.$refs.inputer;
       this.filem = inputDOM.files[0];
@@ -58,7 +50,8 @@ export default {
       })
         .then(successResponse => {
           // console.log( successResponse.data.message);
-          this.showtext = JSON.stringify(successResponse.data, null, "\t");
+          // this.showtext = JSON.stringify(successResponse.data, null, "\t");
+          this.showtext = successResponse.data;
         })
     }
   }
