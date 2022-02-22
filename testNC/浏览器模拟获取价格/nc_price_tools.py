a78988b9591c87297e75dacda194463b8b4c676f
WPP823 <wpp_work_mail@163.com>
2022-02-22 23:33:57 +0800

    修改登陆方法，触发登录时才进行登录，滑动滑块问题还未解决

--- testNC/浏览器模拟获取价格/nc_price_tools.py
+++ testNC/浏览器模拟获取价格/nc_price_tools.py
@@ -5,11 +5,12 @@ import time
 
 from mongoengine import connection, register_connection
 from pyppeteer import launch
-from pz.tools.logger import get_logger
+# from pz.tools.logger import get_logger
 
-from mongo_db.dao.products import ProductDao
-from mongo_db.model.product import ProductModel
-from retrying import retry #设置重试次数用的
+from retrying import retry  # 设置重试次数用的
+
+from my_log import get_logger
+
 
 class PriceToolsPyppeteer():
     def __init__(self, log):
@@ -26,10 +27,13 @@ class PriceToolsPyppeteer():
             # 替换淘宝在检测浏览时采集的一些参数。
             # 就是在浏览器运行的时候，始终让window.navigator.webdriver=false
             # navigator是windiw对象的一个属性，同时修改plugins，languages，navigator 且让
-            await page.evaluate('''() =>{ Object.defineProperties(navigator,{ webdriver:{ get: () => false } }) }''')  # 以下为插入中间js，将淘宝会为了检测浏览器而调用的js修改其结果。
+            await page.evaluate(
+                '''() =>{ Object.defineProperties(navigator,{ webdriver:{ get: () => false } }) }''')  # 以下为插入中间js，将淘宝会为了检测浏览器而调用的js修改其结果。
             await page.evaluate('''() =>{ window.navigator.chrome = { runtime: {},  }; }''')
-            await page.evaluate('''() =>{ Object.defineProperty(navigator, 'languages', { get: () => ['en-US', 'en'] }); }''')
-            await page.evaluate('''() =>{ Object.defineProperty(navigator, 'plugins', { get: () => [1, 2, 3, 4, 5,6], }); }''')
+            await page.evaluate(
+                '''() =>{ Object.defineProperty(navigator, 'languages', { get: () => ['en-US', 'en'] }); }''')
+            await page.evaluate(
+                '''() =>{ Object.defineProperty(navigator, 'plugins', { get: () => [1, 2, 3, 4, 5,6], }); }''')
 
             # await page.screenshot({'path': './1.jpg'})  # 截图保存路径
             # 滑动屏幕
@@ -39,6 +43,7 @@ class PriceToolsPyppeteer():
             iframe_element = await page.Jx('//*[@id="nc_1__scale_text"]/span')
             if iframe_element:
                 self.log.info("出现滑动验证")
+                await mouse_slide(page=page)
 
             # 出现弹出式登录框 #todo
             iframe_element = await page.J('iframe#baxia-dialog-content')
@@ -54,7 +59,10 @@ class PriceToolsPyppeteer():
             #     await page.type('#fm-login-password', 'pzzh123456', {'delay': 120})  # 密码
             #     # await asyncio.sleep(random.randint(1,5)) #login-form > div.fm-btn > button
             #     await page.click('#login-form > div.fm-btn > button')
-
+            login_id = await page.xpath('//*[@id="fm-login-id"]')
+            if login_id:
+                is_login_success = await login_taobao(page=page)
+                self.log.info("PriceToolsPyppeteer.login_taobao is_login_success:{} ".format(is_login_success))
             # 判断价格是否加载
             price_1 = await page.xpath('//*[@id="J_StrPriceModBox"]/dd/span')
             price_2 = await page.xpath('//*[@id="J_PromoPrice"]/dd/div/span')
@@ -73,41 +81,48 @@ class PriceToolsPyppeteer():
         await browser.close()
         return price_list
 
-    async def login_taobao(self):
-        browser = await launch(headless=False, dumpio=True, autoClose=False, userDataDir='./userdata',
-                               args=['--no-sandbox', '--window-size=1920,1080', '--disable-infobars'])  # 进入有头模式
-        page = await browser.newPage()
-        try:
-            await page.setViewport({'width': 1920, 'height': 1080})
-            login_url = 'https://login.taobao.com/member/login.jhtml?redirectURL=https://www.taobao.com/'
-            # await page.goto('https://login.taobao.com/member/login.jhtml')
-            await page.goto(login_url)
-            await page.evaluate('''() =>{ Object.defineProperties(navigator,{ webdriver:{ get: () => false } }) }''')
-            await page.type('#fm-login-id', '比量1234', {'delay': 120})  # 账号
-            # await asyncio.sleep(random.randint(1,5))
-            await page.type('#fm-login-password', 'pzzh123456', {'delay': 120})  # 密码
-            # await asyncio.sleep(random.randint(1,5))
-            await page.click('#login-form > div.fm-btn > button')
-            await asyncio.sleep(random.randint(1, 5))
-        except:
-            await asyncio.sleep(random.randint(1, 5))
-            await browser.close()
-            return False
-        # await page.close()
-        await browser.close()
-        return True
+
+async def login_taobao(page=None):
+    """
+    异步登录方法
+
+    """
+    await asyncio.sleep(2)
+    try:
+        await page.type('#fm-login-id', '化羽而蒙', {'delay': 120})  # 账号
+        # await page.type('#fm-login-id', '比量1234', {'delay': 120})  # 账号
+        # await page.type('#fm-login-password', 'pzzh123456', {'delay': 120})  # 密码
+        await page.type('#fm-login-password', 'w836289789', {'delay': 120})  # 密码
+        await page.click('#login-form > div.fm-btn > button')
+        await asyncio.sleep(random.randint(1, 5))
+    except:
+        # await asyncio.sleep(random.randint(1, 5))
+        # await browser.close()
+        return False
+    # await page.close()
+    # await browser.close()
+    return True
 
 
-def retry_if_result_none(result):
-    return result is None
+# def retry_if_result_none(result):
+#     return result is None
+#
+
+async def iframe_id2frame(iframe_id, page):
+    iframe_element = await page.J(iframe_id)
+    if iframe_element:
+        iframe = await iframe_element.contentFrame()
+        return iframe
+    else:
+        return None
 
 
-@retry(retry_on_result=retry_if_result_none, )
+# @retry(retry_on_result=retry_if_result_none, )
 async def mouse_slide(page=None):
     await asyncio.sleep(2)
     try:
         # 鼠标移动到滑块，按下，滑动到头（然后延时处理），松开按键
-        await page.hover('#nc_1__scale_text > span')  # 不同场景的验证码模块能名字不同。//*[@id="nc_1__scale_text"]/span
+        await page.hover('*[@id="nc_1__scale_text"]/span')  # 不同场景的验证码模块能名字不同。//*[@id="nc_1__scale_text"]/span
         await page.mouse.down()
         await page.mouse.move(2000, 0, {'delay': random.randint(1000, 2000)})
         await page.mouse.up()
@@ -127,8 +142,8 @@ async def mouse_slide(page=None):
 
 
 if __name__ == "__main__":
-    MONGO_HOST_PART = "mongodb://root:Pzzh4Admin@192.168.1.230"  # 230
-    # MONGO_HOST_PART  = "mongodb://root:Pzzh4Admin@dds-wz9db3743e6de5041152-pub.mongodb.rds.aliyuncs.com:3717" # 测试服
+    # MONGO_HOST_PART = "mongodb://root:Pzzh4Adminin@192.168.1.230"  # 230
+    MONGO_HOST_PART = "mongodb://root:Pzzh4Admin@dds-wz9db3743e6de5041152-pub.mongodb.rds.aliyuncs.com:3717"  # 测试服
     # MONGO_HOST_PART  = "mongodb://root:pzzh123456@dds-wz982bab2e6c05b41845-pub.mongodb.rds.aliyuncs.com:3717" # 正式服
     MONGO_HOST_AUTH_DB = "admin"
     MONGO_HOST_REPLICA_SET = None
@@ -136,14 +151,16 @@ if __name__ == "__main__":
     MONGO_HOST = MONGO_HOST_PART + "/" + MONGO_HOST_AUTH_DB
     MONGO_DB_NAME = 'nightcrawler'
 
-    register_connection(db=MONGO_DB_NAME, host=MONGO_HOST_PART, authentication_source=MONGO_HOST_AUTH_DB, replicaset=None, alias=connection.DEFAULT_CONNECTION_NAME)
-    log = get_logger(name='PriceToolsPyppeteer.log', log_path='./')
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(level=logging.INFO)
+    register_connection(db=MONGO_DB_NAME, host=MONGO_HOST_PART, authentication_source=MONGO_HOST_AUTH_DB,
+                        replicaset=None, alias=connection.DEFAULT_CONNECTION_NAME)
+    # log = get_logger(name='PriceToolsPyppeteer.log', log_path='./')
+    log = get_logger()
+    # console_handler = logging.StreamHandler()
+    # console_handler.setLevel(level=logging.INFO)
     # console_handler.setFormatter(logging.Formatter(formatter, datefmt='%Y-%m-%d %H:%M'))
 
     # logger.addHandler(time_rotate_file)
-    log.addHandler(console_handler)
+    # log.addHandler(console_handler)
     # log = log
 
     # log = get_logger(name="PriceToolsPyppeteer.log", log_path="./")
@@ -152,29 +169,32 @@ if __name__ == "__main__":
     add_ret = None
     ext_price_info = None
     # ext_url = "https://item.taobao.com/item.htm?id=619300880358"
-    product_dao = ProductDao(log=log)
-    product_ext_urls = product_dao.get_product_list(fields=[ProductModel.skus.name, ProductModel.product_id.name])
+    ext_url = "https://detail.tmall.com/item.htm?spm=a1z10.3-b-s.w4011-22228839995.44.628e79c7BnwYYA&id=606744628264"
+    # product_dao = ProductDao(log=log)
+    # product_ext_urls = product_dao.get_product_list(fields=[ProductModel.skus.name, ProductModel.product_id.name])
+    product_ext_urls = [ext_url]
 
-    for product in product_ext_urls:
-        product_id = product.product_id
-        for sku in product.skus:
-            ext_url = sku.ext_url
-            sku_id = sku.sku_id
-            log.info(f"product_id:{product_id},sku_id:{sku_id},ext_url:{ext_url}")
-            try:
-                # 重复三次获取无数据时报错
-                for i in range(3):
-                    time.sleep(random.randint(1, 4))
-                    # html_page = asyncio.get_event_loop().run_until_complete(price_tool.login())  # 调用
-                    ext_page_info = asyncio.get_event_loop().run_until_complete(price_tool.get_price(url=ext_url))  # 调用
-                    # price_tool.get_price(url=ext_url)
-                    if ext_page_info:
-                        price = min([int(float(item)) for item in ext_page_info])
-                        log.info(f"product_id:{product_id},sku_id:{sku_id},ext_url:{ext_url}  price:{price}")
-                        break
-                    else:
-                        time.sleep(random.randint(1, 4))
-                        ext_price_info = asyncio.get_event_loop().run_until_complete(price_tool.login_taobao())
-                        log.info("[sync_third_party_product_price][login_taobao][ext_price_info:{}]".format(ext_price_info))
-            except:
-                log.exception("[sync_third_party_product_price_error][ext_url:{}]".format(ext_url))
+    for item in product_ext_urls:
+        # product_id = product.product_id
+        # for sku in product.skus:
+        #     ext_url = sku.ext_url
+        #     sku_id = sku.sku_id
+        #     log.info(f"product_id:{product_id},sku_id:{sku_id},ext_url:{ext_url}")
+        try:
+            # 重复三次获取无数据时报错
+            for i in range(3):
+                time.sleep(random.randint(1, 4))
+                # html_page = asyncio.get_event_loop().run_until_complete(price_tool.login())  # 调用
+                ext_page_info = asyncio.get_event_loop().run_until_complete(price_tool.get_price(url=ext_url))  # 调用
+                # price_tool.get_price(url=ext_url)
+                if ext_page_info:
+                    price = min([int(float(item)) for item in ext_page_info])
+                    log.info(f"product_id:{item}   price:{price}")
+                    # break
+                # # else:
+                # #     time.sleep(random.randint(1, 4))
+                # #     ext_price_info = asyncio.get_event_loop().run_until_complete(price_tool.login_taobao())
+                # #     log.info(
+                #         "[sync_third_party_product_price][login_taobao][ext_price_info:{}]".format(ext_price_info))
+        except:
+            log.exception("[sync_third_party_product_price_error][ext_url:{}]".format(ext_url))
