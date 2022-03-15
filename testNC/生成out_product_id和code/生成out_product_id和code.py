c8c041f0821c936088980aaf2952e41a433aefed
wangying <wpp_work_mail@163.com>
2022-03-15 18:30:30 +0800

    修改生成out_product_id和code 规则

--- testNC/生成out_product_id和code/生成out_product_id和code.py
+++ testNC/生成out_product_id和code/生成out_product_id和code.py
@@ -1,7 +1,8 @@
+import arrow as arrow
 import pandas as pd
 import pypinyin
 
-file_path = './正式服数据0303.xls'
+file_path = './正式服expect_data_0315.xls'
 
 
 def yinjie(word):
@@ -11,34 +12,39 @@ def yinjie(word):
     :param word:
     :return: 例如 xia3oyi2ngua4n
     """
-    s = ''
-    for i in pypinyin.pinyin(word, style=pypinyin.Style.TONE2):
-        s = s + ''.join(i)
-    return f"{s}"
+
+    a = pypinyin.pinyin(word, style=pypinyin.FIRST_LETTER)
+
+    b = []
+    for i in range(len(a)):
+        b.append(str(a[i][0]).upper())
+    c = ''.join(b)
+    return c
+
 
 new_codes = []
-
+now_at = arrow.now(tz="+08:00").format("YYMMDD")
 data = pd.read_excel(file_path).fillna('')
 brands = data.brand
 product_ids = data.product_id.to_list()
 
 out_product_dict = {
-    product_id: 'p_' + '_'.join([yinjie(item) for item in data[data.product_id == product_id].brand.to_list()[0].split(',')]) + f'_{index}'
+    product_id: 'P_' + '_'.join([yinjie(item) for item in data[data.product_id == product_id].brand.to_list()[0].split(',')])+f"_{now_at}" + f'_{index}'
     for index, product_id in enumerate(set(product_ids))
 }
 new_out_product_ids = [out_product_dict[product_id] for product_id in product_ids]
 
 for index, brand_item in enumerate(brands.to_list()):
     if brand_item:
-        new_codes.append('k_' + '_'.join([yinjie(item) for item in brand_item.split(',')]) + f'_{index}')
+        new_codes.append('K_' + '_'.join([yinjie(item) for item in brand_item.split(',')])+f"_{now_at}" + f'_{index}')
     else:
         new_codes.append("")
 
 print(new_out_product_ids)
 print(new_codes)
 
-data.insert(loc=1,column="out_product_id", value=new_out_product_ids)
+data.insert(loc=1,column="spu_code", value=new_out_product_ids)
 data.insert(loc=1, column="code", value=new_codes)
 
-data.to_excel("new.xls")
+data.to_excel("new_spu_code_sku_code.xls")
 pass
